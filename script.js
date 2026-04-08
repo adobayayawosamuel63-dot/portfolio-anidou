a17cabf488f5a0cdbc604e765a50c682e6ba5057
samuel <adobayayawosamuel63@gmail.com>
2026-04-08 04:51:40 +0200

    fin du premier responsive test1

--- script.js
+++ script.js
@@ -36,3 +36,30 @@ tsParticles.load("tsparticles", {
 });
 
 
+// On récupère les éléments
+const btn = document.getElementById('toggleButton');
+const content = document.getElementById('contentBox');
+
+// On ajoute l'événement clic
+btn.addEventListener('click', () => {
+    // La méthode toggle ajoute la classe si elle n'est pas là, 
+    // et la retire si elle est déjà présente.
+    content.classList.toggle('hidden');
+
+    // Optionnel : changer le texte du bouton
+    if (content.classList.contains('hidden')) {
+        btn.textContent = "Afficher le contenu";
+    } else {
+        btn.textContent = "Masquer le contenu";
+    }
+});
+
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', () => {
+    // On ajoute/enlève la classe active au clic
+    hamburger.classList.toggle('active');
+    navLinks.classList.toggle('active');
+});
+
